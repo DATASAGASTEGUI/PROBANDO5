19faf7360602925ab83b0673261e7550b9e3520d
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-05 01:06:26 +0200

    Foto 1

---
+++ src/misrc/MiMetodo.java
@@ -0,0 +1,13 @@
+package misrc;
+
+public class MiMetodo {
+
+    public static double sumar(double n1, double n2) {
+        return n1 + n2;
+    }
+    
+    public static double restar(double n1, double n2) {
+        return n1 - n2;
+    }
+
+}
