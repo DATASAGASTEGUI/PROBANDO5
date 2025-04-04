01671a5fdbe9de69c48fa54346079275866edf4f
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-05 01:18:55 +0200

    Agregar multiplicar

--- src/misrc/MiMetodo.java
+++ src/misrc/MiMetodo.java
@@ -9,5 +9,9 @@ public class MiMetodo {
     public static double restar(double n1, double n2) {
         return n1 - n2;
     }
+    
+    public static double multiplicar(double n1, double n2) {
+        return n1 * n2;
+    }    
 
 }
